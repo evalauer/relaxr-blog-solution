3fac73508128f525cacf52fac67e152ca2e2f906
evalauer <eva.r.lauer@gmail.com>
2018-03-18 15:50:06 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function() {
+	$.hide(".moretext")
+
+	});
+
+
+
+$(".button").on("click", function() {
+alert("We Are Not Currently Taking Signups!");
+$(".button").html("No Sign Ups!");
+
+
+}); 
+
+
+$(".readmore").on("click", function() { 
+$.show(".moretext");
+$(".readmore").html("Read Less");
+
+}); 
+
+$(".readmore").on("click", function() { 
+.hide("lorem, at volutpat orci commodo a. Suspendisse nunc orci, porttitor ac sapien a, rutrum porttitor odio.");
+$(".readmore").html("Read Less");
+
+
+
+
+$(".learnmore").on("click", function() {
+.append("lorem, at volutpat orci commodo a");
+$(".learnmore").html("Learn Less");
+
+}); 
+
+
+$(".learnmore").on("click", function() {
+.hide("lorem, at volutpat orci commodo a");
+$(".learnmore").html("Learn More");
+
+}); 
